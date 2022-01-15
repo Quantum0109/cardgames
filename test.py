e2f25abf0af08d89d8ea3efc85b8bb7fdb575fb5
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2022-01-15 13:39:46 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ import random
 """
 suits = ("червей", "пик", "кресте", "бубен")
 
-def make_deck(suits):
+def make_deck():
     deck = []
     for suit in suits:
         for value in range(6, 15):
@@ -45,28 +45,49 @@ def make_deck(suits):
     return deck
 
 
-def split_deck(deck):
+def split_deck():
     user_deck = deck[:18]
     computer_deck = deck[18:]
     return user_deck, computer_deck
 
-deck = make_deck(suits)
-user_deck = split_deck(deck)[0]
-computer_deck = split_deck(deck)[1]
+deck = make_deck()
+user_deck = split_deck()[0]
+computer_deck = split_deck()[1]
 
 
 
-desk = []
-desk.append(user_deck.pop())
-desk.append(computer_deck.pop())
+table = []
 
-print("игрок выбросил", desk[0]["значение"])
-print("комьютер выбросил", desk[1]["значение"])
+def new_game():
+    while len(user_deck) > 0:
+        table.append(user_deck.pop())
+        table.append(computer_deck.pop())
+
+        print("игрок выбросил", table[0]["значение"])
+        print("комьютер выбросил", table[1]["значение"])
+
+        if table[0]["значение"] > table[1]["значение"]:
+            print("игрок победил")
+            user_deck.reverse()
+            user_deck.append(table)
+            user_deck.reverse()
+            print(len(user_deck))
+            input("--------------------")
+        elif table[0]["значение"] < table[1]["значение"]:
+            print("комп победил")
+            computer_deck.reverse()
+            computer_deck.append(table)
+            computer_deck.reverse()
+            print(len(computer_deck))
+            input("-----------------------")
+        else:
+            print("спор")
+            input("-------------------")
+            new_game()
 
 
-if desk[0]["значение"] > desk[1]["значение"]:
-    print("игрок победил")
-elif desk[0]["значение"] < desk[1]["значение"]:
-    print("комп победил")
-else:
-    print("спор от ubisoft")
+
+make_deck()
+split_deck()
+new_game()
+
